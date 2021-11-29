390a386cdb58c7db6d0acbf2ca4d18c21ef3d6b7
gongyitong <gongyitong@baidu.com>
2021-11-29 14:51:26 +0800

    demo
    Change-Id: I85aedbff4a3c380739de04a55893633f17e4e002

--- main.go
+++ main.go
@@ -8,18 +8,67 @@ modification history
 2021/09/24 16:25:47, by gongyitong@baidu.com, create
 */
 
-// Package main is special.  It defines a
-// standalone executable program, not a library.
-// Within package main the function main is also
-// special—it’s where execution of the program begins.
-// Whatever main does is what the program does.
+
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	//"path/filepath"
+	"strings"
+	"icode.baidu.com/baidu/goodcoder/gongyitong/method"
 )
 
-// main the function where execution of the program begins
+type InputMethod interface {
+	FindWords(string) []string
+}
+
+func loop(im InputMethod) {
+	stdin := bufio.NewReader(os.Stdin)
+	for {
+		spell, err := stdin.ReadString('\n')
+		if err != nil {
+			break
+		}
+		spell = strings.TrimRight(spell, "\n")
+		words := im.FindWords(spell)
+		fmt.Println(strings.Join(words, ", "))
+	}
+}
+//建立全部合法拼音的字典树，用于判断词典名、拼音名是否合法
+func setup() method.TrieTree{
+	var spell_list []string
+	spell, err := os.Open("./pinyin_spell.txt")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		}
+	defer spell.Close()
+
+	br := bufio.NewReader(spell)
+	for {
+		a, _, c := br.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		spell_list = append(spell_list,string(a))
+
+	}
+	spellTree := method.CreateTree(spell_list)
+	return spellTree
+}
+	//var dictpath = "./dict"
+	//var dict_list []string
+	//files,_ := filepath.Glob(dictpath+"/*")
+	//检查是否拼音+，.dat格式
+
+	//fmt.Println(files) // contains a list of all files in the current directory
+
+
 func main() {
-	fmt.Println("Hello, World!")
+	// 若实现加载指定目录下全部词典，并判断词典名是否合法
+    setup()
+
+
 }
